d0118b4b7515861f763afa08d6a757840cfc370f
MilanBratic0512 <milan.bratic2@gmail.com>
2020-06-18 11:40:45 +0200

    completed method for ATM and busines logic in main

--- DAN_XXXIV_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XXXIV_Milan_Bratic/Zadatak_1/Program.cs
@@ -1,19 +1,38 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
     class Program
     {
         static int bank = 10000;
+        static Random rnd = new Random();
+        static readonly object locker = new object();
 
-        static void ATMOne()
+        /// <summary>
+        /// method for raise money from the bank
+        /// </summary>
+        /// <param name="name">client name</param>
+        static void ATM(string name)
         {
-
+            lock (locker)
+            {
+                //random generated money
+                int money = rnd.Next(100, 10000);
+                Console.WriteLine("The {0} client are trying raise {1} dinars", name, money);
+                if (bank < money)
+                {
+                    Console.WriteLine("Sorry {0} there's no money in the bank", name);
+                    return;
+                }
+                bank -= money;
+                if (bank < 0)
+                {
+                    return;
+                }
+                Console.WriteLine("Successfully raised money");
+                Console.WriteLine("{0} dinars left in the bank", bank);
+            }
         }
 
         static void Main(string[] args)
@@ -23,11 +42,14 @@ namespace Zadatak_1
             Console.WriteLine("How many people want to withdraw money from ATM two");
             int atmTwo = int.Parse(Console.ReadLine());
 
-            Thread[] threadsATMOne = new Thread[atmOne];
-            Thread[] threadsATMTwo = new Thread[atmTwo];
-
-
-
+            Thread[] clients = new Thread[atmOne + atmTwo];
+            for (int i = 0; i < clients.Length; i++)
+            {
+                string name = "client " + i + 1;
+                clients[i] = new Thread(() => ATM(name));
+                clients[i].Start();
+            }
+            Console.ReadLine();
         }
     }
 }
