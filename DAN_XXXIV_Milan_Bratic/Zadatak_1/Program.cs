82711fff15ce03ffe774b6e5dbf1cc67c1876ade
MilanBratic0512 <milan.bratic2@gmail.com>
2020-06-18 09:42:21 +0200

    develop ATM machine method

--- DAN_XXXIV_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XXXIV_Milan_Bratic/Zadatak_1/Program.cs
@@ -2,20 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
     class Program
     {
+        static int bank = 10000;
+
+        static void ATMOne()
+        {
+
+        }
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("How many people want to withdraw money from ATM one");
+            int atmOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many people want to withdraw money from ATM two");
+            int atmTwo = int.Parse(Console.ReadLine());
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Thread[] threadsATMOne = new Thread[atmOne];
+            Thread[] threadsATMTwo = new Thread[atmTwo];
+
+
+
         }
     }
 }
