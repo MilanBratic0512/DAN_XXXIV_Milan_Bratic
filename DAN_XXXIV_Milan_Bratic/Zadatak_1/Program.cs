2482e14aabc648610d2b9515816322d235b8c37d
MilanBratic0512 <milan.bratic2@gmail.com>
2020-06-18 11:49:37 +0200

    validations completed

--- DAN_XXXIV_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XXXIV_Milan_Bratic/Zadatak_1/Program.cs
@@ -20,6 +20,7 @@ namespace Zadatak_1
                 //random generated money
                 int money = rnd.Next(100, 10000);
                 Console.WriteLine("The {0} client are trying raise {1} dinars", name, money);
+                
                 if (bank < money)
                 {
                     Console.WriteLine("Sorry {0} there's no money in the bank", name);
@@ -38,18 +39,43 @@ namespace Zadatak_1
         static void Main(string[] args)
         {
             Console.WriteLine("How many people want to withdraw money from ATM one");
-            int atmOne = int.Parse(Console.ReadLine());
+            //validation
+            string input = null;
+            int atmOne = 0;
+            do
+            {
+                input = Console.ReadLine();
+                if (!int.TryParse(input, out atmOne))
+                {
+                    Console.WriteLine("Wrong input!");
+                }
+            } while (!int.TryParse(input, out atmOne));
             Console.WriteLine("How many people want to withdraw money from ATM two");
-            int atmTwo = int.Parse(Console.ReadLine());
-
+            //validation
+            string input2 = null;
+            int atmTwo = 0;
+            do
+            {
+                input2 = Console.ReadLine();
+                if (!int.TryParse(input2, out atmTwo))
+                {
+                    Console.WriteLine("Wrong input!");
+                }
+            } while (!int.TryParse(input2, out atmTwo));
+            //array of threads, represent total number of client
             Thread[] clients = new Thread[atmOne + atmTwo];
             for (int i = 0; i < clients.Length; i++)
             {
+                //generated client name
                 string name = "client " + i + 1;
+                //created thread
                 clients[i] = new Thread(() => ATM(name));
+                //start thread
                 clients[i].Start();
             }
             Console.ReadLine();
         }
+
+        
     }
 }
